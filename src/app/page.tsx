e8276ed5d39a51c8662cdd2e07faa9c3bd497333
Miguel Savignano <m.savignano@stuart.com>
2026-03-21 12:45:53 +0100

    QUESTION_PAGE_PROVIDER

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import config from "@/data/config.json";
 export default async function Home() {
   const { questionsCount, level } = config;
   let questions;
-  try {
-    questions = await getQuestionsFromOllama(questionsCount, level);
-    console.log("Questions fetched from Ollama successfully.");
-  } catch (error) {
-    console.error("Ollama failed, falling back to local questions:");
+  if (process.env.QUESTION_PAGE_PROVIDER === "ollama") {
+    try {
+      questions = await getQuestionsFromOllama(questionsCount, level);
+      console.log("Questions fetched from Ollama successfully.");
+    } catch (error) {
+      console.error("Ollama failed, falling back to local questions:");
+      questions = await getQuestions();
+    }
+  } else {
     questions = await getQuestions();
   }
   return <Quiz allQuestions={questions} />;
